1748dd7ed268e329891baa70dc799abcb6d853da
Rocío Zavala <66143482+rzavala1@users.noreply.github.com>
2023-04-05 13:16:34 -0600

    rzavang
    correcion de error de uri  para apollo

--- src/commons/apollo/index.ts
+++ src/commons/apollo/index.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+
 const apolloClient=new ApolloClient({
-  uri:process.env.NEXT_PUBLIC_GRAPHQL_SERVER_URL,
+  uri:`${process.env.NEXT_PUBLIC_GRAPHQL_SERVER_URL}graphql`,
   cache:new InMemoryCache(),
 });
 
